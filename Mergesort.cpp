44613393cac84e013befc7d506a6651a361aed07
Rouf1224 <ahmadroufr24@gmail.com>
2025-04-17 18:27:10 +0700

    Mergesort 3

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -18,6 +18,6 @@ void mergeSort(int low, int high){
 
     int mid = (low + high) / 2; //step 2
 
-mergesort(low, mid); //step 3a
-mergesort(mid+1, high); //step 3b
+mergeSort(low, mid); //step 3a
+mergeSort(mid+1, high); //step 3b
 }
